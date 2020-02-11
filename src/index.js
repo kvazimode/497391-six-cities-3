732e90edf400064a494b943c17bb125f6bd85165
kvazi <kvazimode2@gmail.com>
2020-02-11 15:09:35 +0300

    Создан компонент для карточки предложения

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom';
 import App from './components/app/app.jsx';
 
 const data = {
-  offerCount: 707
+  offerCount: 707,
+  offerTitles: [`The place you were looking for`, `Barn, but look at this view!`, `Wooden house with free food`, `Beach camping`]
 };
 
 ReactDOM.render(

c74a9fccd3853d49dfcd7720f67e288f263fded2
kvazimode <kvazimode2@gmail.com>
2020-02-16 13:19:43 +0300

    моки для списка предложений

---
+++ src/mocks/offers.js
@@ -0,0 +1,34 @@
+const offerList = [
+  {
+    title: `Beautiful & luxurious apartment at graet location`,
+    imgLink: `img/apartment-01.jpg`,
+    value: 120,
+    type: `Apartment`,
+    isPremium: true,
+    rating: 80
+  },
+  {
+    title: `Wood and stone place`,
+    imgLink: `img/room.jpg`,
+    value: 80,
+    type: `Private room`,
+    isPremium: false,
+    rating: 80
+  },
+  {
+    title: `Canal view Prinsengracht`,
+    imgLink: `img/apartment-02.jpg`,
+    value: 132,
+    type: `Apartment`,
+    isPremium: false,
+    rating: 80
+  },
+  {
+    title: `Nice, cozy, warm big bed apartment`,
+    imgLink: `img/apartment-03.jpg`,
+    value: 180,
+    type: `Apartment`,
+    isPremium: true,
+    rating: 100
+  }
+]

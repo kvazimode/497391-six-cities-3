a78d375be9284abc84dcea7add55ebca5b1716f7
kvazi <kvazimode2@gmail.com>
2020-03-02 21:45:39 +0300

    создан компонент со списком городов

---
+++ src/components/city-list/city-list.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const CityList = (props) => {
+  const {cityList, currentCity, handleCityClick} = props;
+  return <React.Fragment>
+    <h1 className="visually-hidden">Cities</h1>
+    <div className="tabs">
+      <section className="locations container">
+        <ul className="locations__list tabs__list">
+          {cityList.map((city, i) => (
+            <li key={city + i} className="locations__item">
+              <a className={`locations__item-link tabs__item ${currentCity === city ? `tabs__item--active` : ``}`}
+                onClick={() => handleCityClick(city)}
+                href="#">
+                <span>{city}</span>
+              </a>
+            </li>
+          ))}
+        </ul>
+      </section>
+    </div>
+  </React.Fragment>;
+};
+
+CityList.propTypes = {
+  cityList: PropTypes.array.isRequired,
+  currentCity: PropTypes.string.isRequired,
+  handleCityClick: PropTypes.func.isRequired
+};
+
+export default CityList;
--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -2,8 +2,9 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import OfferList from '../offer-list/offer-list.jsx';
 import Map from '../map/map.jsx';
+import CityList from '../city-list/city-list.jsx';
 
-const Main = ({offerList}) => {
+const Main = ({offerList, handleCityClick, currentCity, cityList}) => {
   const offerCount = offerList.length;
 
   return <React.Fragment>
@@ -32,48 +33,12 @@ const Main = ({offerList}) => {
       </header>
 
       <main className="page__main page__main--index">
-        <h1 className="visually-hidden">Cities</h1>
-        <div className="tabs">
-          <section className="locations container">
-            <ul className="locations__list tabs__list">
-              <li className="locations__item">
-                <a className="locations__item-link tabs__item" href="#">
-                  <span>Paris</span>
-                </a>
-              </li>
-              <li className="locations__item">
-                <a className="locations__item-link tabs__item" href="#">
-                  <span>Cologne</span>
-                </a>
-              </li>
-              <li className="locations__item">
-                <a className="locations__item-link tabs__item" href="#">
-                  <span>Brussels</span>
-                </a>
-              </li>
-              <li className="locations__item">
-                <a className="locations__item-link tabs__item tabs__item--active">
-                  <span>Amsterdam</span>
-                </a>
-              </li>
-              <li className="locations__item">
-                <a className="locations__item-link tabs__item" href="#">
-                  <span>Hamburg</span>
-                </a>
-              </li>
-              <li className="locations__item">
-                <a className="locations__item-link tabs__item" href="#">
-                  <span>Dusseldorf</span>
-                </a>
-              </li>
-            </ul>
-          </section>
-        </div>
+        <CityList cityList={cityList} currentCity={currentCity} handleCityClick={handleCityClick}/>
         <div className="cities">
           <div className="cities__places-container container">
             <section className="cities__places places">
               <h2 className="visually-hidden">Places</h2>
-              <b className="places__found">{offerCount} places to stay in Amsterdam</b>
+              <b className="places__found">{offerCount} places to stay in {currentCity}</b>
               <form className="places__sorting" action="#" method="get">
                 <span className="places__sorting-caption">Sort by</span>
                 <span className="places__sorting-type" tabIndex="0">
@@ -107,7 +72,10 @@ Main.defaultProps = {
 };
 
 Main.propTypes = {
-  offerList: PropTypes.array.isRequired
+  offerList: PropTypes.array.isRequired,
+  currentCity: PropTypes.string.isRequired,
+  cityList: PropTypes.array.isRequired,
+  handleCityClick: PropTypes.func.isRequired
 };
 
 export default Main;

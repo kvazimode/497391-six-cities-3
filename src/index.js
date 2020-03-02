f5de6254c461d12adbf80bd3cb84e95ab8725258
kvazi <kvazimode2@gmail.com>
2020-03-02 16:31:38 +0300

    подключение redux к app

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/app.jsx';
-import offerList from './mocks/offers.js';
 import {createStore} from 'redux';
 import {Provider} from 'react-redux';
 import {reducer} from './reducer.js';
@@ -9,11 +8,11 @@ import {reducer} from './reducer.js';
 const store = createStore(
     reducer,
     window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : (f) => f
-    );
+);
 
 ReactDOM.render(
     <Provider store={store}>
-        <App offerList={offerList}/>
+      <App />
     </Provider>,
     document.querySelector(`#root`)
 );
